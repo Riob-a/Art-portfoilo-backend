d03cd11309b8fbc3137dbf2dd3d091d355c439fa
Derrick Rioba Ongwae <riobad74@gmail.com>
2025-07-16 11:39:33 +0300

    added procfile

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///art-portfolio.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///art-portfolio.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
